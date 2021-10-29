4b30ab941c2afe13e91fb8a1f74aab9258b253da
Bridgesniper <92517598+Bridgesniper@users.noreply.github.com>
2021-10-29 15:57:10 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -6,14 +6,25 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './')));
 
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, './komyut_search.html'));
+    response.render('pages/komyut_search', { pageTitle: 'Search | Kompyut.ph'})
 });
 
 app.get('/suggest', (request, response) => {
     response.sendFile(path.join(__dirname, './komyut_suggest.html'));
+});
+
+app.get('/notfound', (request, response) => {
+    response.sendFile(path.join(__dirname, './komyut_notfound.html'));
+});
+
+app.get('/request', (request, response) => {
+    response.sendFile(path.join(__dirname, './komyut_ping.html'));
 });
 
 app.listen(port, () => {
